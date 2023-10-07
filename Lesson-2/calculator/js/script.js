7f867366ca385d19062fce0ad0fbeb59a778060f
Bohdan <bogmira777@gmail.com>
2023-10-07 13:52:57 +0200

    Fix calculator layout add more functions

--- Lesson-2/calculator/js/script.js
+++ Lesson-2/calculator/js/script.js
@@ -10,27 +10,55 @@ const numberButtons = document.querySelectorAll(".btn-number");
 const actionButtons = document.querySelectorAll(".btn-action");
 const calculateButton = document.querySelector(".btn-calculate");
 const clearButton = document.querySelector(".btn-clear");
+const squareButton = document.querySelector(".btn-square");
+const rootButton = document.querySelector(".btn-root");
+
+squareButton.addEventListener("click", (e) => {
+  let input = +output.value;
+  result = Math.pow(input, 2);
+  output.value = result;
+});
+
+rootButton.addEventListener("click", (e) => {
+  let input = +output.value;
+  result = Math.sqrt(input);
+  output.value = result;
+});
 
 numberButtons.forEach((element) => {
   element.addEventListener("click", (e) => {
-    if (output.value == "0") {
-      output.value = element.textContent;
-    } else {
+    if (output.value === "0") {
+      if (element.textContent === ",") {
+        output.value += element.textContent;
+      } else {
+        output.value = element.textContent;
+      }
+    } else if (element.textContent === "," && !output.value.includes(",")) {
       output.value += element.textContent;
+    } else {
+      if (!isNaN(+output.value)) {
+        output.value += element.textContent;
+      }
     }
   });
 });
 
 actionButtons.forEach((element) => {
   element.addEventListener("click", (e) => {
-    value1 = +output.value;
-    action = element.textContent;
-    output.value = 0;
+    let input = +output.value;
+    if (element.textContent === "+/-") {
+      output.value = -input;
+    } else {
+      value1 = input;
+      action = element.textContent;
+      output.value = 0;
+    }
   });
 });
 
 clearButton.addEventListener("click", (e) => {
   output.value = 0;
+  value1 = value2 = result = 0;
 });
 
 calculateButton.addEventListener("click", (e) => {
